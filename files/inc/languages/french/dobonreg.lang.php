a07346dbcb9c3819ec2a7f384fa14cafdd09a458
Eredrim <gilles.moriani@epsi.fr>
2017-06-05 16:17:32 +0200

    Add french translation

---
+++ files/inc/languages/french/dobonreg.lang.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Date of Birth on Registration 1.0.1 - Language File
+
+ * Copyright 2017 Matthew Rogowski
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ ** http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+**/
+
+$l['dobonreg_full_required'] = "Entrez votre date de naissance complète.";
+$l['dobonreg_partial_required'] = "Entrez au minimum votre jour et mois de naissance.";
+$l['dobonreg_optional'] = "Ce champ est facultatif.";
+
+$l['userdata_invalid_dob_need_full'] = "Veuillez entrer votre date de naissance complète.";
+$l['userdata_invalid_dob_empty'] = "Veuillez entrer au minimum le jour et le mois de votre date de naissance.";
+
+$l['invalid_dob_age_reg'] = "Vous êtes trop jeune pour vous enregistrer sur ce forum. L'age miminium requis est de {1} ans.";
+$l['invalid_dob_age_view'] = "Vous êtes trop jeune pour naviguer sur ce forum. L'age miminium requis est de {1} ans.";
+?>
